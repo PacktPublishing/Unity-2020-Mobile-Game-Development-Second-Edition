90e18e9e7d8ca5a6d69fbc92088cd87bba2c625c
John P. Doran <john@johnpdoran.com>
2020-07-29 02:56:12 -0500

    Added Chapter 4 files for Unity 2020

---
+++ Chapter 4/Assets/Scripts/MainMenuBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // LoadScene 
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    /// <summary> 
+    /// Will load a new scene upon being called 
+    /// </summary> 
+    /// <param name="levelName">The name of the level we want 
+    /// to go to</param> 
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Chapter 4/Assets/Scripts/PauseScreenBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // SceneManager 
+
+public class PauseScreenBehaviour : MainMenuBehaviour
+{
+
+    public static bool paused;
+
+    [Tooltip("Reference to the pause menu object to turn on/off")]
+    public GameObject pauseMenu;
+
+    /// <summary> 
+    /// Reloads our current level, effectively "restarting" the     
+    /// game 
+    /// </summary> 
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary> 
+    /// Will turn our pause menu on or off 
+    /// </summary> 
+    /// <param name="isPaused"></param> 
+    public void SetPauseMenu(bool isPaused)
+    {
+        paused = isPaused;
+
+        // If the game is paused, timeScale is 0, otherwise 1 
+        Time.timeScale = (paused) ? 0 : 1;
+        pauseMenu.SetActive(paused);
+    }
+
+    private void Start()
+    {
+        // Must be reset in Start or else game will be paused upon restart
+        SetPauseMenu(false);
+    }
+
+}
