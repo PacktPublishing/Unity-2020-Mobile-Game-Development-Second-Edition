e65d40e283471aa58894d8cbfce949fac367c33a
John P. Doran <john@johnpdoran.com>
2020-07-30 02:19:51 -0500

    Chapter 6 files added for Unity 2020

---
+++ Chapter 6/Assets/Scripts/MainMenuBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // LoadScene 
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    /// <summary> 
+    /// Will load a new scene upon being called 
+    /// </summary> 
+    /// <param name="levelName">The name of the level we want 
+    /// to go to</param> 
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+
+        if (UnityAdController.showAds)
+        {
+            // Show an ad 
+            UnityAdController.ShowAd();
+        }
+    }
+
+    public void DisableAds()
+    {
+        UnityAdController.showAds = false;
+
+        // Used to store that we shouldn't show ads
+        PlayerPrefs.SetInt("Show Ads", 0);
+    }
+
+    protected virtual void Start()
+    {
+        // Initialize the showAds variable
+        UnityAdController.showAds = (PlayerPrefs.GetInt("Show Ads", 1) == 1);
+    }
+}
